da94f8729be8afd2eef50a30f09472a1fe287c68
Cameron Buss <camnb97@gmail.com>
2022-08-01 21:32:06 -0700

    edited homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "./image/logo/white_logo_dark_background.jpg";
 
 function Homepage() {
     return (
         <div>
-            <h1>Hey guys! I'm a header!</h1>
+            <h1>
+                <img src={logo} className="App-logo" alt="logo" />
+                Hey guys! I'm a header!</h1>
             <h2>h2</h2>
             <ul>
                 <li>ii1</li>
