a4cc8365c29945491f8e9e0ad8edbccf60c6d875
Nikolas <kolemazur@gmail.com>
2022-08-03 17:56:59 -0600

    Create GraphQL endpoints/Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 // import new logo
 // import logo from './logo.svg';
 import Homepage from "./components/homepage";
 import './App.css';
 
+// GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
+    <ApolloClient client={client}>
     <div className="App">
       <Homepage></Homepage>
       {/* <header className="App-header">
@@ -22,6 +53,7 @@ function App() {
         </a>
       </header> */}
     </div>
+    </ApolloClient>
   );
 }
 
