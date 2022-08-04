a0d5a8454fee6e4bb38f417135311713a42b0411
Cameron Buss <camnb97@gmail.com>
2022-08-03 19:53:51 -0700

    edited homepage

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,28 +1,14 @@
 import React from "react";
-// import logo1 from "./image/logo_black_bg";
+import {
+    Input
+} from 'semantic-ui-react'
 
-var style = {
-    div: {
-        "height": "500px",
-        "backgroundColor": "black",
-    }
-}
+const InputExampleFocus = () => <Input focus placeholder='Search...' />
 
 function Homepage() {
     return (
         <div>
-            <h1>
-                {/* <img src={logo1} className="App-logo" alt="logo" /> */}
-                Hey guys! I'm a header!</h1>
-            <h2>h2</h2>
-            <ul>
-                <li>ii1</li>
-                <li>li2</li>
-                <li>li3</li>
-            </ul>
-            <div style={style.div} >
-
-            </div>
+            <div class="ui focus input"><input type="text" placeholder="Search..." /></div>
         </div>
     );
 }
