c8ab78a346f8613a95644b38ddd4073ef73cf6aa
Cameron Buss <camnb97@gmail.com>
2022-08-06 11:30:15 -0700

    ..

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -11,8 +11,12 @@ function Header() {
                     {/* <Image src={logo} /> */}
                     Rent-A-Dev
                 </Menu.Item>
-                <Menu.Item position="right" ><NavLink to="/login">Log In</NavLink></Menu.Item>
-                <Menu.Item><NavLink to="/signup">Sign Up</NavLink ></Menu.Item>
+                <Menu.Item position="right" >
+                    <NavLink to="/login">Log In</NavLink>
+                </Menu.Item>
+                <Menu.Item>
+                    <NavLink to="/signup">Sign Up</NavLink >
+                </Menu.Item>
                 <Menu.Item>Profile</Menu.Item>
             </Menu>
         </div>
