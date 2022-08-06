60a26d2c0e3cd7286e87e853a4b540ed40646f35
Nikolas <kolemazur@gmail.com>
2022-08-06 10:40:02 -0600

    Add LOGIN mutation/reformat ADD_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,19 +23,36 @@ export const CREATE_VOTE = gql`
 `;
 
 export const ADD_USER = gql`
-mutation addUser(
-  $username: String!, 
-  $email: String!, 
-  $password: String!
-  ) {
-    addUser(
-    username: $username, 
-    email: $email, 
-    password: $password
+  mutation addUser(
+    $username: String!, 
+    $email: String!, 
+    $password: String!
     ) {
-      token
-      user {
-        _id
+      addUser(
+      username: $username, 
+      email: $email, 
+      password: $password
+      ) {
+        token
+        user {
+          _id
+      }
     }
   }
-}`
+`;
+
+export const LOGIN = gql`
+  mutation login(
+    $email: String!, 
+    $password: String!
+    ) {
+      login(email: $email, 
+      password: $password
+      ) {
+        token
+        user {
+          _id
+        }
+      }
+    }
+`;
