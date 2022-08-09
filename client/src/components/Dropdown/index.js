f69659be195b8c2c9ca750dc037bad56d41ab438
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-08 23:30:29 -0700

    removed unnecessary dependancies and introduced fixed dataset

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,17 +1,24 @@
-import _ from "lodash";
 import React from "react";
 import { Dropdown } from "semantic-ui-react";
 
-const addressDefinitions = faker.definitions.address;
-const skillOptions = _.map(addressDefinitions.state, (skills, index) => ({
-  key: addressDefinitions.state_abbr[index],
-  text: skills,
-  value: addressDefinitions.state_abbr[index],
-}));
+// const addressDefinitions = faker.definitions.address;
+// const skillOptions = _.map(addressDefinitions.skills, (skills, index) => ({
+//   key: addressDefinitions.state_abbr[index],
+//   text: skills,
+//   value: addressDefinitions.state_abbr[index],
+// }));
+
+const skillOptions = [
+  {
+    key: "HTML",
+    text: "HTML",
+    value: "HTML",
+  },
+];
 
 const DropMenu = () => (
   <Dropdown
-    placeholder="Skills"
+    placeholder="Please Select Relevant Skills"
     fluid
     multiple
     search
