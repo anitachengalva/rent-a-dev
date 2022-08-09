3bf123c866d6943f722b89ee6fab8a7908c88c6d
Nikolas <kolemazur@gmail.com>
2022-08-08 20:13:34 -0600

    Correct previous push

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -68,38 +68,3 @@ export const REMOVE_USER_SKILL = gql`
     }
   }
 `;
-
-export const ADD_USER = gql`
-  mutation addUser(
-    $username: String!, 
-    $email: String!, 
-    $password: String!
-    ) {
-      addUser(
-      username: $username, 
-      email: $email, 
-      password: $password
-      ) {
-        token
-        user {
-          _id
-      }
-    }
-  }
-`;
-
-export const LOGIN = gql`
-  mutation login(
-    $email: String!, 
-    $password: String!
-    ) {
-      login(email: $email, 
-      password: $password
-      ) {
-        token
-        user {
-          _id
-        }
-      }
-    }
-`;
