957c74af66ff5bca4ee8955c704412cf34387f3d
Nikolas <kolemazur@gmail.com>
2022-08-08 21:44:16 -0600

    Fix typo

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,17 @@
 const db = require('../config/connection');
-const { User, Requests } = require('../models');
+const { User, Request } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const requestSeeds = require('./requestSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Requests.deleteMany({});
+    await Request.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
     for (let i = 0; i < requestSeeds.length; i++) {
-      const { _id, requestSender } = await Requests.create(requestSeeds[i]);
+      const { _id, requestSender } = await Request.create(requestSeeds[i]);
       const user = await User.findOneAndUpdate(
         { username: requestSender },
         {
