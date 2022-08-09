9e5d65e6d9265d52b6bc1c2b72db50a02b0d541d
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-08 23:21:27 -0700

    removed unnecessary imported dependency and adjust skillOptions data set

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,12 +1,11 @@
-import faker from "faker";
 import _ from "lodash";
 import React from "react";
 import { Dropdown } from "semantic-ui-react";
 
 const addressDefinitions = faker.definitions.address;
-const stateOptions = _.map(addressDefinitions.state, (state, index) => ({
+const skillOptions = _.map(addressDefinitions.state, (skills, index) => ({
   key: addressDefinitions.state_abbr[index],
-  text: state,
+  text: skills,
   value: addressDefinitions.state_abbr[index],
 }));
 
