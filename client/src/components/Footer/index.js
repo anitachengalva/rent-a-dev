57cc3290f8bd80dce9a46091022514354ae72fcb
Zane Pisano <pisanoz@live.com>
2022-08-10 20:13:03 -0700

    added linkedin to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,26 +4,9 @@ const Footer = () => {
 	return (
 		<footer className="">
 			<div className="">
-				<Image src={image} />
 				<section className="linkedinPros">
 					<div
-						class="badge-base LI-profile-badge"
-						data-locale="en_US"
-						data-size="medium"
-						data-theme="light"
-						data-type="VERTICAL"
-						data-vanity="zane-pisano-a24177230"
-						data-version="v1"
-					>
-						<a
-							class="badge-base__link LI-simple-link"
-							href="https://www.linkedin.com/in/zane-pisano-a24177230?trk=profile-badge"
-						>
-							zane pisano
-						</a>
-					</div>
-					<div
-						class="badge-base LI-profile-badge"
+						className="badge-base LI-profile-badge"
 						data-locale="en_US"
 						data-size="medium"
 						data-theme="light"
@@ -32,14 +15,14 @@ const Footer = () => {
 						data-version="v1"
 					>
 						<a
-							class="badge-base__link LI-simple-link"
+							className="badge-base__link LI-simple-link"
 							href="https://www.linkedin.com/in/anitachengalva?trk=profile-badge"
 						>
 							Anita Chengalva
 						</a>
 					</div>
 					<div
-						class="badge-base LI-profile-badge"
+						className="badge-base LI-profile-badge"
 						data-locale="en_US"
 						data-size="medium"
 						data-theme="light"
@@ -48,10 +31,42 @@ const Footer = () => {
 						data-version="v1"
 					>
 						<a
-							class="badge-base__link LI-simple-link"
+							className="badge-base__link LI-simple-link"
 							href="https://www.linkedin.com/in/cameronbuss-webdev?trk=profile-badge"
 						>
 							Cameron Buss
+						</a>
+					</div>
+					<div
+						className="badge-base LI-profile-badge"
+						data-locale="en_US"
+						data-size="medium"
+						data-theme="light"
+						data-type="VERTICAL"
+						data-vanity="trentyn-powell-862265173"
+						data-version="v1"
+					>
+						<a
+							className="badge-base__link LI-simple-link"
+							href="https://www.linkedin.com/in/trentyn-powell-862265173?trk=profile-badge"
+						>
+							Trentyn Powell
+						</a>
+					</div>
+					<div
+						className="badge-base LI-profile-badge"
+						data-locale="en_US"
+						data-size="medium"
+						data-theme="light"
+						data-type="VERTICAL"
+						data-vanity="zane-pisano-a24177230"
+						data-version="v1"
+					>
+						<a
+							className="badge-base__link LI-simple-link"
+							href="https://www.linkedin.com/in/zane-pisano-a24177230?trk=profile-badge"
+						>
+							zane pisano
 						</a>
 					</div>
 				</section>
