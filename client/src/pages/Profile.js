1966164dfaf56d8cf74786f23bbfdd10b012d50c
Cameron Buss <camnb97@gmail.com>
2022-08-10 19:22:45 -0700

    ..

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,8 +5,8 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-// import ThoughtForm from '../components/ThoughtForm';
-// import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
+import ThoughtList from '../components/ThoughtList';
 
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
@@ -15,36 +15,35 @@ import Auth from '../utils/auth';
 import { } from 'semantic-ui-react';
 
 const Profile = () => {
-    // const { username: userParam } = useParams();
+    const { username: userParam } = useParams();
 
-    // const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    //     variables: { username: userParam },
-    // });
+    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+        variables: { username: userParam },
+    });
 
-    // const user = data?.me || data?.user || {};
-    // // navigate to personal profile page if username is yours
-    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    //     // naviagte to something different?
-    //     return <Navigate to="/me" />;
-    // }
+    const user = data?.me || data?.user || {};
+    // navigate to personal profile page if username is yours
+    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+        // naviagte to something different?
+        return <Navigate to="/me" />;
+    }
 
-    // if (loading) {
-    //     return <div>Loading...</div>;
-    // }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-    // if (!user?.username) {
-    //     return (
-    //         <h4>
-    //             You need to be logged in to see this. Use the navigation links above to
-    //             sign up or log in!
-    //         </h4>
-    //     );
-    // }
+    if (!user?.username) {
+        return (
+            <h4>
+                You need to be logged in to see this. Use the navigation links above to
+                sign up or log in!
+            </h4>
+        );
+    }
 
     return (
         <div>
-            <p>hey</p>
-            {/* <div className="flex-row justify-center mb-3">
+            <div className="flex-row justify-center mb-3">
                 <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
                     Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
@@ -65,7 +64,7 @@ const Profile = () => {
                         <ThoughtForm />
                     </div>
                 )}
-            </div> */}
+            </div>
         </div>
     );
 };
