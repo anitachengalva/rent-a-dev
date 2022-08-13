f054773846c4c8badaab6ec774c4ea520efc114f
Nikolas <kolemazur@gmail.com>
2022-08-13 09:29:04 -0600

    Refactor basic code segments

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,11 +4,9 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
-
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import RequestForm from '../components/RequestForm/index';
 
 import Auth from '../utils/auth';
 
@@ -24,7 +22,6 @@ const Profile = () => {
     const user = data?.me || data?.user || {};
     // navigate to personal profile page if username is yours
     if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-        // naviagte to something different?
         return <Navigate to="/me" />;
     }
 
@@ -42,26 +39,40 @@ const Profile = () => {
     }
 
     return (
-        <div>
-            <div className="flex-row justify-center mb-3">
-                <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+        <div className="centered">
+            <div className="">
+                <h2 className="">
                     Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
+                
+            <div className="">
+                <p>
+                    {userParam ? `${user.skills}` : 'your'}
+                </p>
+            </div>
 
-                <div className="col-12 col-md-10 mb-5">
-                    <ThoughtList
-                        thoughts={user.thoughts}
-                        title={`${user.username}'s thoughts...`}
-                        showTitle={false}
-                        showUsername={false}
-                    />
-                </div>
+            <div className="">
+                <p>
+                    User Description
+                </p>
+                <br />
+                <p>
+                {userParam ? `${user.description}` : 'your'}
+                </p>
+            </div>
+
+            <div className="">
+                <p>
+                {userParam ? `${user.email}` : 'your'}
+                </p>
+            </div>
+
                 {!userParam && (
                     <div
-                        className="col-12 col-md-10 mb-3 p-3"
+                        className=""
                         style={{ border: '1px dotted #1a1a1a' }}
                     >
-                        <ThoughtForm />
+                        <RequestForm />
                     </div>
                 )}
             </div>
