dedd31556377c01fb306f0eceb616d4bb5a8f3ac
Cameron Buss <camnb97@gmail.com>
2022-08-12 22:22:10 -0700

    ..

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,31 +11,31 @@ import DropMenu from "../components/Dropdown/index";
 
 const Home = () => {
 
-	const { username: userParam } = useParams();
+	// const { username: userParam } = useParams();
 
-	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-		variables: { username: userParam },
-	});
+	// const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+	// 	variables: { username: userParam },
+	// });
 
-	const user = data?.me || data?.user || {};
-	// navigate to personal profile page if username is yours
-	if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-		// naviagte to something different?
-		return <Navigate to="/home" />;
-	}
+	// const user = data?.me || data?.user || {};
+	// // navigate to personal profile page if username is yours
+	// if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+	// 	// naviagte to something different?
+	// 	return <Navigate to="/home" />;
+	// }
 
-	if (loading) {
-		return <div>Loading...</div>;
-	}
+	// if (loading) {
+	// 	return <div>Loading...</div>;
+	// }
 
-	if (!user?.username) {
-		return (
-			<h4>
-				You need to be logged in to see this. Use the navigation links above to
-				sign up or log in!
-			</h4>
-		);
-	}
+	// if (!user?.username) {
+	// 	return (
+	// 		<h4>
+	// 			You need to be logged in to see this. Use the navigation links above to
+	// 			sign up or log in!
+	// 		</h4>
+	// 	);
+	// }
 	//this was copied _________________________________
 	// which mutations will be displayed on the hmepage?
 	// -users snippets, skills dropdown, "welcome <user name>",
