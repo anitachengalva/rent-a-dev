b6122bcb1fb85c69e309e2738e23ee0a95b00eb6
Trentyn <trentynpowell@gmail.com>
2022-08-12 21:38:15 -0700

    LinkedIn cards

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,78 +1,85 @@
-import React from "react";
-
-const Footer = () => {
-	return (
-		<footer className="">
-			<div className="">
-				<section className="linkedinPros">
-					<div
-						className="badge-base LI-profile-badge"
-						data-locale="en_US"
-						data-size="medium"
-						data-theme="light"
-						data-type="VERTICAL"
-						data-vanity="anitachengalva"
-						data-version="v1"
-					>
-						<a
-							className="badge-base__link LI-simple-link"
-							href="https://www.linkedin.com/in/anitachengalva?trk=profile-badge"
-						>
-							Anita Chengalva
-						</a>
-					</div>
-					<div
-						className="badge-base LI-profile-badge"
-						data-locale="en_US"
-						data-size="medium"
-						data-theme="light"
-						data-type="VERTICAL"
-						data-vanity="cameronbuss-webdev"
-						data-version="v1"
-					>
-						<a
-							className="badge-base__link LI-simple-link"
-							href="https://www.linkedin.com/in/cameronbuss-webdev?trk=profile-badge"
-						>
-							Cameron Buss
-						</a>
-					</div>
-					<div
-						className="badge-base LI-profile-badge"
-						data-locale="en_US"
-						data-size="medium"
-						data-theme="light"
-						data-type="VERTICAL"
-						data-vanity="trentyn-powell-862265173"
-						data-version="v1"
-					>
-						<a
-							className="badge-base__link LI-simple-link"
-							href="https://www.linkedin.com/in/trentyn-powell-862265173?trk=profile-badge"
-						>
-							Trentyn Powell
-						</a>
-					</div>
-					<div
-						className="badge-base LI-profile-badge"
-						data-locale="en_US"
-						data-size="medium"
-						data-theme="light"
-						data-type="VERTICAL"
-						data-vanity="zane-pisano-a24177230"
-						data-version="v1"
-					>
-						<a
-							className="badge-base__link LI-simple-link"
-							href="https://www.linkedin.com/in/zane-pisano-a24177230?trk=profile-badge"
-						>
-							zane pisano
-						</a>
-					</div>
-				</section>
-			</div>
-		</footer>
-	);
-};
-
-export default Footer;
+import React, { useEffect } from "react";
+export default function Footer() {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://platform.linkedin.com/badges/js/profile.js";
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+    return (
+        <footer className="">
+            <div className="">
+                <section className="bios">
+                    <div
+                        className="badge-base LI-profile-badge"
+                        data-locale="en_US"
+                        data-size="medium"
+                        data-theme="light"
+                        data-type="VERTICAL"
+                        data-vanity="anitachengalva"
+                        data-version="v1"
+                    >
+                        <a
+                            className="badge-base__link LI-simple-link"
+                            href="https://www.linkedin.com/in/anitachengalva?trk=profile-badge"
+                        >
+                            Anita Chengalva
+                        </a>
+                    </div>
+                    <div
+                        className="badge-base LI-profile-badge"
+                        data-locale="en_US"
+                        data-size="medium"
+                        data-theme="light"
+                        data-type="VERTICAL"
+                        data-vanity="cameronbuss-webdev"
+                        data-version="v1"
+                    >
+                        <a
+                            className="badge-base__link LI-simple-link"
+                            href="https://www.linkedin.com/in/cameronbuss-webdev?trk=profile-badge"
+                        >
+                            Cameron Buss
+                        </a>
+                    </div>
+                    <div
+                        className="badge-base LI-profile-badge"
+                        data-locale="en_US"
+                        data-size="medium"
+                        data-theme="light"
+                        data-type="VERTICAL"
+                        data-vanity="trentyn-powell-862265173"
+                        data-version="v1"
+                    >
+                        <a
+                            className="badge-base__link LI-simple-link"
+                            href="https://www.linkedin.com/in/trentyn-powell-862265173?trk=profile-badge"
+                        >
+                            Trentyn Powell
+                        </a>
+                    </div>
+                    <div
+                        className="badge-base LI-profile-badge"
+                        data-locale="en_US"
+                        data-size="medium"
+                        data-theme="light"
+                        data-type="VERTICAL"
+                        data-vanity="zane-pisano-a24177230"
+                        data-version="v1"
+                    >
+                        <a
+                            className="badge-base__link LI-simple-link"
+                            href="https://www.linkedin.com/in/zane-pisano-a24177230?trk=profile-badge"
+                        >
+                            zane pisano
+                        </a>
+                    </div>
+                </section>
+            </div>
+        </footer>
+    );
+}
